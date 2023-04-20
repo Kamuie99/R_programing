25245d2836d5e9c0c8fee5fb77e0285d73d823be
병아리 개발자 <101976498+Kamuie99@users.noreply.github.com>
2023-04-20 10:39:34 +0900

    Add files via upload

---
+++ 20180290.R
@@ -0,0 +1,7 @@
+df_name <- data.frame( name = c("Lee", "Park", "So", "Kim"),
+                       age = c(55, 47, 35, 26),
+                       height = c(170, 162, 166, 159))
+df_name
+
+mean(df_name$age)
+mean(df_name$height)
